c3f87edeae3c6f95a320cf2e4d66d6f8bdc86a7a
DeepakSalla <Salladeepak1@gmail.com>
2025-07-05 04:30:37 +0530

    Final working Cypress QA project with local app and CI setup

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,17 @@
-module.exports = {
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      // Log terminal output during CI runs
       on('task', {
         log(message) {
-          console.log(message)
+          console.log('CYPRESS LOG:', message)
           return null
         }
       })
-    }
+    },
+    video: true,
+    screenshotOnRunFailure: true
   }
-}
+})
